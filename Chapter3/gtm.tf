ba0c197f8f915dade91d4093d831fc263b3de71a
EC2 Default User <ec2-user@ip-172-31-90-64.ec2.internal>
2018-09-23 20:35:30 +0000

    instances and gtm

---
+++ Chapter3/gtm.tf
@@ -0,0 +1,8 @@
+# Global Traffic Management using DNS
+resource "aws_route53_record" "www" {
+  zone_id = "${aws_route53_zone.book.zone_id}"
+  name    = "www.book.cloudpatterns.cuk"
+  type    = "A"
+  ttl     = "300"
+  records = ["${aws_instance.cheap_worker.public_ip}","${aws_instance.cheap_worker_west.public_ip}"]
+}
---
+++ Chapter3/instances.tf
@@ -0,0 +1,38 @@
+# Virginia
+resource "aws_instance" "cheap_worker" {
+  # the below ami is for the latest Bitnami Wordpress East
+  ami           = "ami-001e1c1159ccfe992"
+  instance_type = "t2.micro"
+  availability_zone  = "us-east-1d"
+  associate_public_ip_address = true
+
+  tags {
+    Name = "CheapWorker"
+  }
+}
+output "id" {
+  value = "${aws_instance.cheap_worker.id}"
+}
+output "ip" {
+  value = "${aws_instance.cheap_worker.public_ip}"
+}
+
+# Oregon
+resource "aws_instance" "cheap_worker_west" {
+  # the below ami is for the latest Bitnami Wordpress West
+  ami           = "ami-000ce50ab0df5943f"
+  provider = "aws.west"
+  instance_type = "t2.micro"
+  availability_zone  = "us-west-1d"
+  associate_public_ip_address = true
+
+  tags {
+    Name = "CheapWorker"
+  }
+}
+output "id_west" {
+  value = "${aws_instance.cheap_worker_west.id}"
+}
+output "ip_west" {
+  value = "${aws_instance.cheap_worker_west.public_ip}"
+}
