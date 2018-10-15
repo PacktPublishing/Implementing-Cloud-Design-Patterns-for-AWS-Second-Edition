680ec513b10b3fa6e428a8f567429fb8616248a4
Sean Keery <sean@seankeery.com>
2018-10-15 02:12:00 +0000

    post-plan fixes

--- Chapter5/private.tf
+++ Chapter5/private.tf
@@ -15,12 +15,11 @@ resource "aws_nat_gateway" "cloudpatterns_nat_gw" {
 }
 
 resource "aws_route_table" "nat_route_table" {
-  name = "VPC-Private"
   vpc_id = "${var.vpc_id}"
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_nat_gateway.cloudpatterns_nat_gw.gateway_id}"
+    gateway_id = "${aws_nat_gateway.cloudpatterns_nat_gw.id}"
   }
   
 }
