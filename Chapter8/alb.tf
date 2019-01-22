6e8024d0487d1a7228bb4af1a2b9a405820804e6
Sean Keery <sean@seankeery.com>
2019-01-22 07:40:02 +0000

    debug alb lambda

--- Chapter8/alb.tf
+++ Chapter8/alb.tf
@@ -5,6 +5,17 @@ resource "aws_lb" "lambda-alb" {
   subnets            = ["subnet-447dd623","subnet-476f170d"]
 }
 
+resource "aws_lb_listener" "alb_listener" {  
+  load_balancer_arn = "${aws_lb.lambda-alb.arn}"  
+  port              = "80"  
+  protocol          = "HTTP"
+  
+  default_action {    
+    target_group_arn = "${aws_lb_target_group.lambda-target-group.arn}"
+    type             = "forward"  
+  }
+}
+
 resource "aws_lb_target_group" "lambda-target-group" {
   name        = "alb-lambda-group"
   target_type = "lambda"
--- Chapter8/alb_lambda.js
+++ Chapter8/alb_lambda.js
@@ -1,10 +1,10 @@
 'use strict';
 
 exports.handler = (event, context, callback) => {
-    const request = event.Records[0].cf.request;
+    const request = event;
     const headers = request.headers;
 
-    if (request.uri !== '/') {
+    if (request.path !== '/') {
         // do not process if this is not an A-B test request
         callback(null, request);
         return;
@@ -16,18 +16,12 @@ exports.handler = (event, context, callback) => {
     const pathExperimentB = '/indexB.html';
 
     let experimentUri;
-    if (headers.cookie) {
-        for (let i = 0; i < headers.cookie.length; i++) {
-            if (headers.cookie[i].value.indexOf(cookieExperimentA) >= 0) {
-                console.log('Experiment A cookie found');
-                experimentUri = pathExperimentA;
-                break;
-            } else if (headers.cookie[i].value.indexOf(cookieExperimentB) >= 0) {
-                console.log('Experiment B cookie found');
-                experimentUri = pathExperimentB;
-                break;
-            }
-        }
+    if (headers.cookie == cookieExperimentB ) {
+        console.log('Experiment B cookie found');
+        experimentUri = pathExperimentB;
+    } else {
+        console.log('No valid cookie found');
+        experimentUri = pathExperimentA;
     }
 
     if (!experimentUri) {
